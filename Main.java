8522e12b80d2d786d8574bf160f0eb8fd380e1e7
Sarthak Sahu <vishalsahu5@gmail.com>
2016-06-18 08:05:08 +0000

    Created 2D array version to generate the soln

--- Main.java
+++ Main.java
@@ -1,49 +1,49 @@
-import java.util.*;
 public class Main 
 {
 
 	public static void main(String args[])
 	{
-		/*
-		 * Initializing Grid: This grid array is ryt now a 1D array but we have to find a way to represent
-		 * all the possible sudoku solutions. That is, we need to get it in the 2D array. Build it on this
-		 * logic.
-		 */
-		int grid[] = new int[9];
+		int grid[][] = new int[9][9];
 		int i = 0;   //Variable to be used only for the iteration purposes.
-		/*
-		 * The Loop below generates non repeating nos in random order from 1-9.
-		 * The loop should be modified in such a way that we get this in 2 D array form. Also, Brute
-		 * Force has been used in the approach to generate random nos. If possible come up with a better method.
-		 */
-		while(i != 9)
+		int j = 0;
+		for(i = 0; i<9; i++)
 		{
-			int flag = 0;
-			int key = (int) (9*Math.random() + 1);
-			for(int j=0; j<9; j++)
+			j=0;
+			while(j != 9)
 			{
-				if(key == grid[j])     
+				int flag = 0;
+				int key = (int) (9*Math.random() + 1);
+				for(int k=0; k<9; k++)
 				{
-					flag = 1;
-					break;
+					//row check
+					if(key == grid[i][k])     
+					{
+						flag = 1;
+						break;
+					}
+					//column check
+					if(key == grid[k][j])
+					{
+						flag = 1;
+						break;
+					}
+					//box check
+					for(int x = (j/3)*3; x < ((j/3)*3)+3; x++)
+						for(int y = (i/3)*3; y < ((i/3)*3)+3; y++)
+							if(key == grid[x][y])
+							{
+								flag = 1;
+								break;
+							}
+				}
+				if(flag == 0)
+				{
+					grid[i][j] = key;
+					System.out.print(grid[i][j]+" ");
+					j++;
 				}
 			}
-			if(flag == 0)
-			{
-				grid[i] = key;
-				i++;
-			}
+			System.out.println("");
 		}
-		/*
-		 * Logic used in above loop is that we are checking for pre-existing element in the grid and re
-		 * iterating to get another element.
-		 */
-		
-		// Printing out the 1D grid.
-		for(i=0; i<9; i++)
-		{
-			System.out.print(grid[i]+" ");
-		}
-			
 	}
 }
